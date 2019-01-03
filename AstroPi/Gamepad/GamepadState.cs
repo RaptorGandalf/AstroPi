45302cd8a2dac88ed103fa257d76e97621c0bbc6
James Hogle <jhogle117@gmail.com>
2019-01-02 23:07:03 -0600

    getting this party started, what with some input processing and audio expiriments

---
+++ AstroPi/Gamepad/GamepadState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Gaming.Input;
+
+namespace AstroPi
+{
+    public class GamepadState
+    {
+        public bool Connected { get; set; }
+        public GamepadReading Reading { get; set; }
+        public string StateLog { get; set; }
+
+        public string StatusText
+        {
+            get
+            {
+                return Connected ? "Gamepad connected!" : "No gamepad connected!";
+            }
+        }
+
+        public string LeftJoystickCoordinates
+        {
+            get
+            {
+                if(Connected)
+                {
+                    return GetCoordinates(Reading.LeftThumbstickX, Reading.LeftThumbstickY);
+                }
+
+                return "0, 0";
+            }
+        }
+
+        public string RightJoystickCoordinates
+        {
+            get
+            {
+                if (Connected)
+                {
+                    return GetCoordinates(Reading.RightThumbstickX, Reading.RightThumbstickY);
+                }
+
+                return "0, 0";
+            }
+        }
+
+        private string GetCoordinates(double x, double y)
+        {
+            x = Math.Round(x, 1);
+            y = Math.Round(y, 1);
+
+            return $"{x}, {y}";
+        }
+    }
+}
---
+++ AstroPi/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace AstroPi
+{
+    public sealed partial class MainPage : Page
+    {
+        public GamepadState GamepadState;
+
+        private GamepadManager _gamepadManager;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            _gamepadManager = new GamepadManager(InputRecieved);
+
+            GamepadState = new GamepadState
+            {
+                Connected = false
+            };
+
+        }
+
+        private async void InputRecieved(GamepadState state)
+        {
+            GamepadState = state;
+
+            //just putting this here to try audio
+            if(state.Reading.Buttons == Windows.Gaming.Input.GamepadButtons.A)
+            {
+                var soundDirectory = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets\Audio\Astromech-Sounds");
+
+                var files = await soundDirectory.GetFilesAsync();
+
+                var generator = new Random();
+
+                var fileIndex = generator.Next(0, files.Count - 1);
+
+                var file = files[fileIndex];
+
+                var mediaPlayer = new MediaPlayer
+                {
+                    Source = MediaSource.CreateFromStorageFile(file),
+                };
+
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    //await Task.Delay(1);
+                    mediaPlayer.Play();
+
+                  //  Task.Delay(5000);
+                });
+
+                //mediaPlayer.Dispose();
+            }
+
+            await UpdateInputUI();
+        }
+
+        private async Task UpdateInputUI()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                GamepadStatusText.Text = GamepadState.StatusText;
+                LtsText.Text = GamepadState.LeftJoystickCoordinates;
+                RtsText.Text = GamepadState.RightJoystickCoordinates;
+                ButtonsText.Text = GamepadState.Reading.Buttons.ToString();
+            });
+        }
+    }
+}
